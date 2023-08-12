75031cc093908c3ee0a077afb0194b635fd84f25
shkim <shk940607@gmail.com>
2023-08-12 17:39:17 +0900

    Spring Container Apply Complete Commit

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.memeber.MemberRepository;
 import hello.core.memeber.MemberService;
 import hello.core.memeber.MemberServiceImpl;
@@ -14,21 +17,27 @@ import hello.core.order.OrderServiceImpl;
  * @Date 2023. 8. 12.
  * 관심사 분리 (DI 의존관계 주입) 
  */
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 	
+	@Bean
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 	
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 	
+	@Bean
 	public DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
+		// return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 }
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,10 @@
 package hello.core;
 
+import java.io.ObjectInputFilter.Config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.memeber.Grade;
 import hello.core.memeber.Member;
 import hello.core.memeber.MemberService;
@@ -12,8 +17,15 @@ import hello.core.memeber.MemberServiceImpl;
  */
 public class MemberApp {
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
-		MemberService memberSerivce = appConfig.memberService();
+//		AppConfig appConfig = new AppConfig();
+//		MemberService memberSerivce = appConfig.memberService();
+		
+		
+		/* AppConfig가 annotation(@Configuration, @Bean) 기반으로 설정 되어 있기 때문에 AnnotaionConfiguration으로 지정 */
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		/* @Bean의 경우 기본적으로 Method 이름으로 Bean 이름을 설정한다. */
+		MemberService memberSerivce = applicationContext.getBean("memberService", MemberService.class);
 		
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberSerivce.join(member);
