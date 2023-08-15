dab13cb0c741b2967d1e80c35aa51d120cee74df
shkim <shk940607@gmail.com>
2023-08-15 15:53:02 +0900

    Component Scan End Commit And TDD

---
+++ core/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,19 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author sh.kim
+ * @Date 2023. 8. 15.
+ * 해당 컴포넌트가 붙은 Class는 Component Scan 대상
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+	
+}
