805489bd0fde9125362ee1a4fbf10a102edee232
shkim <shk940607@gmail.com>
2023-08-26 01:28:58 +0900

    allBean Tdd commit

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,5 +1,7 @@
 package hello.core.autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +27,13 @@ public class AllBeanTest {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
 		DiscountService discountService = ac.getBean(DiscountService.class);
 		Member member = new Member(1L, "userA", Grade.VIP);
-		int discountPrice = discountService.d
+		int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+		
+		assertThat(discountService).isInstanceOf(DiscountService.class);
+		assertThat(discountPrice).isEqualTo(1000);
+		
+		int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+		assertThat(rateDiscountPrice).isEqualTo(2000);
 		
 	}
 	
@@ -40,6 +48,16 @@ public class AllBeanTest {
 			
 			System.out.println("policyMap = " + policyMap);
 			System.out.println("policy = " + policy);
+		}
+		
+		
+		/**
+		 * discountCode로 넘어온 인터페이스의 구현체로 실행 됨.
+		 */
+		public int discount (Member member, int price, String discountCode) {
+			DiscountPolicy discountPolicy = policyMap.get(discountCode);
+			return discountPolicy.discount(member, price);
+			
 		}
 	}
 }
